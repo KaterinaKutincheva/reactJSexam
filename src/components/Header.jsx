c4b03770edc290bf1f3b9399147be35c18b9fa56
Katerina Kutincheva <k.g.kutincheva@gmail.com>
2025-04-08 14:46:58 +0300

    Adjusted logo in header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,11 +10,10 @@ export default function Header() {
         <header className="site-header">
             <div className="header-content">
                 <img
-                    src="https://dl.dropboxusercontent.com/scl/fi/sed8yz64g18i1qc8nddel/web-logo.svg?rlkey=zmf26mhlw1pgbavmw0zuzrnfe"
+                    src="/web-logo.svg"
                     alt="KNews Logo"
                     className="logo"
                 />
-                <h1 className="site-title">KNews 24/7</h1>
             </div>
 
             <nav>
